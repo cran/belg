a068c77b091a4d4b70717258f2be738f38203a5d
Jakub Nowosad <nowosad.jakub@gmail.com>
2022-05-01 13:30:10 +0000

    version 1.5.2

---
+++ inst/doc/belg2.R
@@ -0,0 +1,10 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  echo = FALSE
+)
+
+## ---- fig.width=6-------------------------------------------------------------
+knitr::include_graphics("summary.png")
+
